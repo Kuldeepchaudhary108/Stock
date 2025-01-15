0adbf09cfb3ad0d6279bdc0b1d5668c264209a2d
kuldeepchaudhary75 <choudharykuldeep5511@gmail.com>
2025-01-15 15:20:18 +0530

    debuging in vercel

--- frontend/src/components/pages/index.js
+++ frontend/src/components/pages/index.js
@@ -5,7 +5,7 @@ import VideoGrid from "./Cards/VideoCard";
 import Modules from "./modules/Module";
 import VideoModule from "./modules/VideoModule";
 import BlogList from "./modules/BlogList";
-import VarsityCertified from "./modules/certified";
+import VarsityCertified from "./modules/Certified";
 import TextModule from "./modules/TextModule";
 import Login from "./Login/Login";
 import Signup from "./Signup/signup";
